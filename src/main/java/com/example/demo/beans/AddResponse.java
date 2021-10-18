1717a7ff59ba7d6e4f678fdc53d49dd7fd9eae4a
reshailalvi <reshailalvi@hotmail.com>
2021-10-19 03:31:09 +0500

    Add all of the project files

---
+++ src/main/java/com/example/demo/beans/AddResponse.java
@@ -0,0 +1,22 @@
+package com.example.demo.beans;
+
+public class AddResponse {
+
+	String message;
+	int id;
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
---
+++ src/main/java/com/example/demo/services/CustomerService.java
@@ -0,0 +1,63 @@
+package com.example.demo.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.beans.AddResponse;
+import com.example.demo.beans.CustomerPojo;
+import com.example.demo.repository.CustomerRepository;
+
+@Service
+public class CustomerService {
+
+	@Autowired
+	CustomerRepository repo;
+
+	public List<CustomerPojo> Home() {
+
+		return repo.findAll();
+
+	}
+
+	public CustomerPojo getCustomerById(int id) {
+		return repo.findById(id).get();
+	}
+
+	public CustomerPojo addCustomer(CustomerPojo b) {
+		b.setId(getSize());
+		repo.save(b);
+		return b;
+	}
+
+	public int getSize() {
+		return repo.findAll().size() + 1;
+	}
+
+	public AddResponse deleteCustomer(int id) {
+		repo.deleteById(id);
+		AddResponse res = new AddResponse();
+
+		res.setId(id);
+		res.setMessage("Customer deleted successfully...");
+
+		return res;
+	}
+
+	public CustomerPojo getCustomerByName(String name) {
+		List<CustomerPojo> customers = repo.findAll();
+		CustomerPojo cus = null;
+		for (CustomerPojo i : customers) {
+			if (i.getCustomerName().equalsIgnoreCase(name)) {
+				cus = i;
+			}
+		}
+
+		return cus;
+	}
+
+	public CustomerPojo updateCustomer(CustomerPojo b) {
+		repo.save(b);
+		return b;
+	}
+
+}
