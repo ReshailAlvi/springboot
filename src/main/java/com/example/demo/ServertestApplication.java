29f09447206317676e926cfac5de8b76420c4747
ReshailAlvi <61703682+ReshailAlvi@users.noreply.github.com>
2021-10-23 16:01:27 +0500

    Update ServertestApplication.java
    Fix for CORS failure.

--- src/main/java/com/example/demo/ServertestApplication.java
+++ src/main/java/com/example/demo/ServertestApplication.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class ServertestApplication {
+public class StartApplication {
 
+	
+	
 	public static void main(String[] args) {
-		SpringApplication.run(ServertestApplication.class, args);
+		 final ApplicationContext ctx =SpringApplication.run(StartApplication.class, args);
 	}
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                
+            }
+        };
+    }
 }
