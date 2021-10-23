45d59de9f98080d602c5e18b63f394c90dd63926
ReshailAlvi <61703682+ReshailAlvi@users.noreply.github.com>
2021-10-23 16:02:06 +0500

    Update CustomerController.java
    Update controller class

--- src/main/java/com/example/demo/controllers/CustomerController.java
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.example.demo.beans.AddResponse;
 import com.example.demo.beans.CustomerPojo;
 import com.example.demo.services.CustomerService;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class CustomerController {
 
@@ -24,7 +26,7 @@ public class CustomerController {
 
 	@GetMapping("/getcustomers")
 	public List<CustomerPojo> NavigateToHome() {
-		return service.Home();
+		return service.getallcustomers();
 	}
 
 	@GetMapping("/getcustomer/{id}")
@@ -48,8 +50,14 @@ public class CustomerController {
 	}
 
 	@PostMapping("/addcustomer")
-	public CustomerPojo addCustomer(@RequestBody CustomerPojo b) {
-		return service.addCustomer(b);
+	public ResponseEntity<CustomerPojo> addCustomer(@RequestBody CustomerPojo b) {
+
+		try {
+			CustomerPojo cus = service.addCustomer(b);
+			return new ResponseEntity<CustomerPojo>(cus, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 
 	@DeleteMapping("/deletecustomer/{id}")
