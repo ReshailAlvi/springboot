b5c83c8bba5b06f0dfd13ef0f31ffb951e3039c9
ReshailAlvi <61703682+ReshailAlvi@users.noreply.github.com>
2021-10-23 15:59:53 +0500

    Update ServertestApplicationTests.java
    Updated to fix CORS error.

--- src/test/java/com/example/demo/ServertestApplicationTests.java
+++ src/test/java/com/example/demo/ServertestApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Arrays;
 
-@SpringBootTest
-class ServertestApplicationTests {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-	@Test
-	void contextLoads() {
+@SpringBootApplication
+public class StartApplication {
+
+	
+	
+	public static void main(String[] args) {
+		 final ApplicationContext ctx =SpringApplication.run(StartApplication.class, args);
 	}
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                
+            }
+        };
+    }
 }
